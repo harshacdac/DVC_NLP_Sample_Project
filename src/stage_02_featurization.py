1afe8bcd4e083517c7c03e87764eb2ac50f9a0b4
harshacdac <harshacdac@gmail.com>
2021-10-31 21:49:40 +0530

    stage 02 convert to lower string

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -3,7 +3,8 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, get_df
+import numpy as np
 
 STAGE = "Two"
 
@@ -26,6 +27,14 @@ def main(config_path, params_path):
     create_directories([featurized_data_dir_path])
     featurized_train_data_path= os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
     featurized_test_data_path= os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TEST"])
+
+    max_features=params["featurize"]["max_features"]
+    ngrams=params["featurize"]["ngrams"]
+
+    df_train= get_df(train_data_path)
+
+    train_words= np.array(df_train.text.str.lower().values.astype("U"))
+    print(train_words)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
